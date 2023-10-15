a78b25190a132a4630124691f411a7f9703c48c9
mayjoy <cillomayjoybutao@gmail.com>
2023-10-15 19:57:37 +0800

    fourth

--- assets/js/main.js
+++ assets/js/main.js
@@ -250,7 +250,7 @@
 
 
 // Array of keywords
-const keywords = ['Gallery', 'About', 'Services', 'Team', 'Blog', 'Portfolio-Details', 'Python', 'Index', 'Portfolio'];
+const keywords = ['Gallery', 'About', 'Services', 'Team', 'Blog', 'Index', 'Portfolio'];
 
 // Function to redirect to a page
 function redirect(keyword) {
